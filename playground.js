5674165de7915820dc46bfe77521842beeae9f49
Somprasong Damyos <somprasong@hospital-os.com>
2018-11-06 12:32:07 +0700

    if-else

--- playground.js
+++ playground.js
@@ -3,13 +3,21 @@ var age = 4;
 var isSterilized = true;
 
 function petInfo(petName, petAge, petIsSterilized) {
+  var sterilized = '';
+
+  if (petIsSterilized) {
+    sterilized = 'Yes';
+  } else {
+    sterilized = 'False';
+  }
+
   return (
     'Pet name is ' +
     petName +
     ', age is ' +
     petAge +
     ' and pet is sterilized: ' +
-    petIsSterilized
+    sterilized
   );
 }
 
