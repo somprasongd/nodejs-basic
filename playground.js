76f8c3c5762f953c93e8a4cc6b60d90bda2d9934
Somprasong Damyos <somprasong@hospital-os.com>
2018-11-06 13:47:10 +0700

    rest & spread operator

--- playground.js
+++ playground.js
@@ -1,17 +1,36 @@
 const pets = ['Stamp', 'Snow'];
 
-pets.push('Slim');
-console.log(pets);
+// pets.push('Slim');
+// console.log(pets);
 
-for (let pet of pets) {
-  console.log(pet);
+// for (let pet of pets) {
+//   console.log(pet);
+// }
+
+// console.log(pets.map(pet => 'Name: ' + pet));
+// console.log(pets);
+
+// console.log(pets.filter(pet => pet.length > 4));
+// console.log(pets);
+
+// console.log(pets.reduce((prev, curr) => prev + ', ' + curr ));
+// console.log(pets);
+
+// spread array
+const copiedArray = [...pets];
+console.log(copiedArray);
+
+// spread object
+const pet = {
+  name: 'Stamp',
+  age: 4
+};
+const copiesObject = { ...pet };
+console.log(copiesObject);
+
+// rest array
+const toArray = (...args) => {
+  return args;
 }
 
-console.log(pets.map(pet => 'Name: ' + pet));
-console.log(pets);
-
-console.log(pets.filter(pet => pet.length > 4));
-console.log(pets);
-
-console.log(pets.reduce((prev, curr) => prev + ', ' + curr ));
-console.log(pets);
+console.log(toArray(1, 2, 3, 4, 5));
