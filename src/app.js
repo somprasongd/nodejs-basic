189a7fe7a76a4e00fdac6f137de65dc6386641be
Somprasong Damyos <somprasong@hospital-os.com>
2018-11-06 17:42:21 +0700

    use anonymous function

--- src/app.js
+++ src/app.js
@@ -1,10 +1,8 @@
 const http = require('http');
 
-const handlerRequest = (req, res) => {
+const server = http.createServer((req, res) => {
   res.write('<h1>Hello from node.js server!</h1>');
   res.end();
-}
-
-const server = http.createServer(handlerRequest);
+});
 
 server.listen(3000, () => console.log('Server started.'));
