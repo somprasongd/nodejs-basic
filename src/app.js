2a8f1c37f1513aa30f1255715727aa29f14fb62a
Somprasong Damyos <somprasong@hospital-os.com>
2018-11-07 16:21:30 +0700

    path and query params

--- src/app.js
+++ src/app.js
@@ -19,6 +19,20 @@ app.delete('/user', function (req, res) {
   res.send('Got a DELETE request at /user');
 });
 
+// path parameters
+app.get('/greeting/:name', (req, res) => {
+  console.log(req.params);
+  const {name} = req.params;
+  res.send(`Hello ${name}`);
+});
+
+// query parameters
+app.get('/greeting', (req, res) => {
+  console.log(req.query);
+  const {name} = req.query;
+  res.send(`Hello ${name}`);
+});
+
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}!`);
