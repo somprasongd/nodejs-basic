3278e6bfeb8eec1d9f53c5678063734c8594f1f6
Somprasong Damyos <somprasong@hospital-os.com>
2018-11-07 15:21:55 +0700

    using nodemon

--- src/app.js
+++ src/app.js
@@ -1,3 +1,11 @@
-const {add} = require('./lib');
+const express = require('express');
+const app = express();
+const port = 3000;
 
-console.log(add(1, 2));
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}!`)
+});
